49843ca13efc96fe235d5819c7619de12a6e4919
lenor21 <dejesusronel29@gmail.com>
2022-09-18 23:49:13 +0800

    modified index.html && new file index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+function evenOrOdd(number) {
+  if (number % 2 == 0) {
+    console.log("The number is even");
+  } else {
+    console.log("The number is odd");
+  }
+}
+
+evenOrOdd(window.prompt("Input a number"));
