22795f414fbd7c2400c25d749500adbd5eac4acb
lenor21 <dejesusronel29@gmail.com>
2022-09-19 00:20:48 +0800

    modified index.js

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+const input = window.prompt("Input a number");
+
 function evenOrOdd(number) {
   if (number % 2 == 0) {
     console.log("The number is even");
-  } else {
+  } else if (number % 1 == 0) {
     console.log("The number is odd");
+  } else if (typeof input === "string") {
+    console.log("Invalid!");
   }
 }
 
-evenOrOdd(window.prompt("Input a number"));
+evenOrOdd(input);
