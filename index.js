28b57f108b7021335a600963752ecfb1c64b59be
lenor21 <dejesusronel29@gmail.com>
2022-09-19 00:42:02 +0800

    modified

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
-const input = window.prompt("Input a number");
+const result = document.querySelector("#result");
+const myInput = document.querySelector("#myInput");
+const myButton = document.querySelector("#myButton");
+const reset = document.querySelector("#reset");
 
-function evenOrOdd(number) {
-  if (number % 2 == 0) {
-    console.log("The number is even");
-  } else if (number % 1 == 0) {
-    console.log("The number is odd");
-  } else if (typeof input === "string") {
-    console.log("Invalid!");
+myButton.addEventListener("click", () => {
+  if (myInput.value % 2 == 0) {
+    result.textContent = `Result: ${"Even"}`;
+  } else if (myInput.value % 1 == 0) {
+    result.textContent = `Result: ${"Odd"}`;
+  } else if (typeof myInput.value === "string") {
+    result.textContent = `Result: ${"Invalid!"}`;
   }
-}
+});
 
-evenOrOdd(input);
+reset.addEventListener("click", () => {
+  result.textContent = `Result:`;
+  myInput.value = "";
+});
+
+// const input = window.prompt("Input a number");
+
+// function evenOrOdd(number) {
+//   if (number % 2 == 0) {
+//     console.log("The number is even");
+//   } else if (number % 1 == 0) {
+//     console.log("The number is odd");
+//   } else if (typeof input === "string") {
+//     console.log("Invalid!");
+//   }
+// }
+
+// evenOrOdd(input);
